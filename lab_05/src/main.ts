a1a1a439d395bac500feda2e53b0595fd8d70d69
oleojake <oleojake@gmail.com>
2024-03-05 16:37:28 +0100

    lab_05 revision tutoria

--- lab_05/src/main.ts
+++ lab_05/src/main.ts
@@ -6,161 +6,114 @@ let jugadas = [];
 const elementPointsInfo = document.getElementById("points-info");
 const elementPoints = document.getElementById("points");
 
-// MOSTRAR PUNTUACIÓN
-function muestraPuntuacion () : void {
-if (elementPointsInfo !== null && elementPointsInfo !== undefined &&
-    elementPoints !==null && elementPoints !== undefined){
-        if(queHubieraPasadoPulsado == false) {
-            if ( puntos > 7.5){
-                elementPointsInfo.innerHTML = `💀 ¡GAME OVER, te has pasado!`;
-                elementPoints.innerHTML = "PUNTUACIÓN: " + puntos.toString();
-                gameOver();
-            }
-            else {
-                elementPoints.innerHTML = "PUNTUACIÓN: " + puntos.toString();
-            }
-        }
-        else { // Hace la simulación pero no cuenta como nueva jugada
-            if ( puntos > 7.5) {
-                elementPointsInfo.innerHTML = `💀 ¡Te hubieras pasado, con ${puntos} puntos!`;
-            }
-            else {
-                elementPointsInfo.innerHTML = `🍀 ¡Lástima, Hubieras llegado a ${puntos} puntos!`;
-            }
-        }
-    }
+const dameNumeroAleatorio = () => {
+    return Math.ceil(Math.random() * 10);
 }
-document.addEventListener("DOMContentLoaded", muestraPuntuacion);
 
-// DAME CARTA
-function dameCarta() : void {
-    let cartaSeleccionada = Math.ceil(Math.random() * 10); // Entre 1 y 10
-    if (cartaSeleccionada > 7) {
-        cartaSeleccionada = cartaSeleccionada +2; // Si es 8, 9 o 10 le suma 2 => 10, 11 y 12
+const dameNumeroDeCarta = (numeroAleatorio : number) : number => {
+    if (numeroAleatorio > 7) {
+        return numeroAleatorio +2;
     }
-    mostrarCarta(cartaSeleccionada);
+    return numeroAleatorio;
 }
-const botonDameCarta = document.getElementById("boton-dame-carta");
-botonDameCarta?.addEventListener("click", dameCarta);
 
-// MOSTRAR CARTA
-function mostrarCarta (carta : number) : void {
-    let imgCarta = document.getElementById("card");
+const dameSrcCarta = (carta : number) : string => {
     switch (carta) {
-        case 0:
-            if (imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
-                puntos = puntos + carta;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;
         case 1:
-            if (imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
-                puntos = puntos + carta;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
         case 2:
-            if(imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
-                puntos = puntos + carta;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
         case 3:
-            if(imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
-                puntos = puntos + carta;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
         case 4:
-            if(imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
-                puntos = puntos + carta;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
         case 5:
-            if(imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
-                puntos = puntos + carta;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
         case 6:
-            if(imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
-                puntos = puntos + carta;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
         case 7:
-            if(imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
-                puntos = puntos + carta;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
         case 10:
-            if(imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
-                puntos = puntos + 0.5;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
         case 11:
-            if(imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
-                puntos = puntos + 0.5;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
         case 12:
-            if(imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
-                imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
-                puntos = puntos + 0.5;
-                jugadas.push(carta);
-                actualizaHistorial(imgCarta.src);
-                muestraPuntuacion();
-            }
-            break;           
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
+        default:
+            return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
     }
+}
+
+const dameLosPuntosDeLaCarta = (numeroDeCarta : number) : number => {
+    if (numeroDeCarta <= 7) {
+        return numeroDeCarta;
+    } else {
+        return 0.5;
+    }
+}
+
+const sumaPuntos = (numeroDeCarta : number) => {
+    puntos = puntos + numeroDeCarta
+}
+
+const mostrarCarta = (srcCarta : string) : void  => {
+    const imgCarta = document.getElementById("card");
+    if (imgCarta !== null && imgCarta !== undefined && imgCarta instanceof HTMLImageElement) {
+        imgCarta.src = srcCarta;
+    }
+}
+
+const registrarJugadas = (puntosAnotados : number) => {
+    jugadas.push (puntosAnotados);
+}
+
+const obtenerIdCartaHistorial = () : string => {
+    return `card${jugadas.length}`;
 }
 
 // HISTORIAL 
-function actualizaHistorial (src : string) : void {
-    const cartaHistorial = document.getElementById(`card${jugadas.length}`);
-    if(cartaHistorial !== null && cartaHistorial !== undefined &&
-        cartaHistorial instanceof HTMLImageElement){
-            cartaHistorial.src = src;
-            cartaHistorial.style.opacity = "1";
-        }
+const muestraCartaEnHistorial = (numeroDeCarta : number) : void => {
+    const id = obtenerIdCartaHistorial();
+    const cartaAPintarHistorial = document.getElementById(id);
+    if(cartaAPintarHistorial !== null && cartaAPintarHistorial !== undefined &&
+        cartaAPintarHistorial instanceof HTMLImageElement){
+            const srcCarta = dameSrcCarta (numeroDeCarta);
+            cartaAPintarHistorial.src = srcCarta;
+            cartaAPintarHistorial.style.opacity = "1";
+    }
 }
 
-// GAME OVER
-function gameOver () {
+const revisarPartida = () => {
+    if (puntos > 7.5) {
+        muestraMensajeGameOver ();
+    }
+    else {
+        actualizaPuntuación ();
+    }
+}
+// La primera vez se llama cuando el DOM está listo
+document.addEventListener("DOMContentLoaded", revisarPartida);
+
+// BUCLE PRINCIPAL
+const dameCarta = () : void => {
+    const numeroAleatorio = dameNumeroAleatorio();
+    const numeroDeCarta = dameNumeroDeCarta (numeroAleatorio);
+    const srcCarta = dameSrcCarta (numeroDeCarta);
+    mostrarCarta(srcCarta);
+    const puntosDeLaCarta = dameLosPuntosDeLaCarta (numeroDeCarta);
+    sumaPuntos (puntosDeLaCarta);
+    registrarJugadas(puntosDeLaCarta);
+    muestraCartaEnHistorial(numeroDeCarta);
+    revisarPartida();
+}
+const botonDameCarta = document.getElementById("boton-dame-carta");
+if (botonDameCarta !== null && botonDameCarta !== undefined
+    && botonDameCarta instanceof HTMLButtonElement) {
+        botonDameCarta.addEventListener("click", dameCarta);
+}
+
+// Desactiva Botón Dame Carta
+const desactivaDameCarta = () => {
     if (botonDameCarta !== null && botonDameCarta !== undefined && botonDameCarta instanceof HTMLButtonElement && 
         botonNuevaPartida !== null && botonNuevaPartida !== undefined && botonNuevaPartida instanceof HTMLButtonElement && 
         botonPlantarme !== null && botonPlantarme !== undefined && botonPlantarme instanceof HTMLButtonElement) {
@@ -170,14 +123,38 @@ function gameOver () {
     }
 }
 
-// NUEVA PARTIDA
-function nuevaPartida () {
+const muestraMensajeGameOver = () => {
+if (elementPointsInfo !== null && elementPointsInfo !== undefined &&
+    elementPoints !==null && elementPoints !== undefined) {
+        if (queHubieraPasadoPulsado == false) {
+            elementPointsInfo.innerHTML = `💀 ¡GAME OVER, te has pasado!`;
+            elementPoints.innerHTML = "PUNTUACIÓN: " + puntos.toString();
+            desactivaDameCarta();
+        }
+        else {
+            elementPointsInfo.innerHTML = `💀 ¡Te hubieras pasado, con ${puntos} puntos!`;
+        }
+    }
+}
+
+const actualizaPuntuación = () => {
+if (elementPointsInfo !== null && elementPointsInfo !== undefined &&
+    elementPoints !==null && elementPoints !== undefined) {
+        if (queHubieraPasadoPulsado == false) {
+            elementPoints.innerHTML = "PUNTUACIÓN: " + puntos.toString();
+        }
+        else {
+            elementPointsInfo.innerHTML = `🍀 ¡Lástima, Hubieras llegado a ${puntos} puntos!`;
+        }
+    }
+}               
+
+const restablecerJuego = () => {
     if (botonDameCarta !== null && botonDameCarta !== undefined && botonDameCarta instanceof HTMLButtonElement && 
         botonNuevaPartida !== null && botonNuevaPartida !== undefined && botonNuevaPartida instanceof HTMLButtonElement && 
         botonPlantarme !== null && botonPlantarme !== undefined && botonPlantarme instanceof HTMLButtonElement &&
         botonHubieraPasado !== null && botonHubieraPasado !== undefined && botonHubieraPasado instanceof HTMLButtonElement &&
         elementPointsInfo !== null && elementPointsInfo !== undefined) {
-            // Se restablece el juego
             botonDameCarta.disabled = false;
             botonPlantarme.style.display = "inline";
             botonNuevaPartida.style.display = "none";
@@ -185,29 +162,56 @@ function nuevaPartida () {
             queHubieraPasadoPulsado = false;
             puntos = 0;
             elementPointsInfo.innerHTML = "ℹ️ Cartas del 1-7 y las figuras 0.5 puntos";
-            muestraPuntuacion ();
-            mostrarCarta (puntos); // Como se pasa el 0 mostrarCarta muestra el reverso
-            borrarHistorial ();
         }
 }
-const botonNuevaPartida = document.getElementById("boton-nueva-partida");
-botonNuevaPartida?.addEventListener("click",nuevaPartida);
+
+const vaciaJugadasAnteriores = () => {
+    jugadas = [];
+}
+
+const borraCartaHistorial = (idCarta : string) => {
+let cartaHistorial = document.getElementById(idCarta);
+    if(cartaHistorial !== null && cartaHistorial !== undefined &&
+    cartaHistorial instanceof HTMLImageElement){
+        cartaHistorial.style.opacity = "0";
+        const srcCarta = dameSrcCarta(0); // El 0 pinta reverso
+        cartaHistorial.src = srcCarta;
+    }
+}
 
 // BORRAR HISTORIAL
-function borrarHistorial () : void {
-    jugadas = []; // Vacía el array de jugadas
-    for (let i = 1; i <= 8; i++){
-        let cartaHistorial = document.getElementById(`card${i}`);
-        if(cartaHistorial !== null && cartaHistorial !== undefined &&
-        cartaHistorial instanceof HTMLImageElement){
-            cartaHistorial.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
-            cartaHistorial.style.opacity = "0";
-        }
+const borrarHistorial = () : void => {
+vaciaJugadasAnteriores();
+for (let i = 1; i <= 8; i++){
+    borraCartaHistorial (`card${i}`);
+}
+}
+
+// NUEVA PARTIDA
+const nuevaPartida = () => {
+    restablecerJuego();
+    const srcCarta = dameSrcCarta(0); // El 0 pinta reverso
+    mostrarCarta (srcCarta);
+    borrarHistorial ();
+    revisarPartida ();  
+}
+const botonNuevaPartida = document.getElementById("boton-nueva-partida");
+if (botonNuevaPartida !== null && botonNuevaPartida !== undefined
+    && botonNuevaPartida instanceof HTMLButtonElement) {
+        botonNuevaPartida.addEventListener("click",nuevaPartida);
+}
+
+
+const muestraBotonQueHubieraPasado = () => {
+    if (botonHubieraPasado!== null && botonHubieraPasado !== undefined) {
+        // Muestra el botón qué hubiera pasado y desactiva el resto de botones
+        botonHubieraPasado.style.display = "block";
+        desactivaDameCarta();
     }
 }
 
 // ME PLANTO
-function mePlanto () : void {
+const mePlanto = () : void => {
     if (elementPointsInfo !== null && elementPointsInfo !== undefined &&
         elementPoints !==null && elementPoints !== undefined) {
         if (puntos === 7.5) {
@@ -223,27 +227,27 @@ function mePlanto () : void {
             elementPointsInfo.innerHTML = "🥱 Has sido muy conservador";
         }
     }
-    if (botonHubieraPasado!== null && botonHubieraPasado !== undefined) {
-        botonHubieraPasado.style.display = "block";
-        gameOver();
-    }
+
+    muestraBotonQueHubieraPasado();
 
 }
 const botonPlantarme = document.getElementById("boton-plantarme");
-botonPlantarme?.addEventListener("click", mePlanto);
+if (botonPlantarme !== null && botonPlantarme !== undefined
+    && botonPlantarme instanceof HTMLButtonElement) {
+        botonPlantarme.addEventListener("click", mePlanto);
+    }
 
 // QUE HUBIERA PASADO
-function queHubieraPasado () {
+const queHubieraPasado = () => {
     if (botonHubieraPasado !== null && botonHubieraPasado !== undefined && botonHubieraPasado instanceof HTMLButtonElement) {
+        // Hace la simulación pero mostrará otro mensaje y no actualizará puntos
         botonHubieraPasado.style.display = "none";
         queHubieraPasadoPulsado = true;
-        // Hace la simulación pero como se ha marcado a true, cuando llegue a muestra puntuación no será una nuevajugada
         dameCarta(); 
     }
 }
 const botonHubieraPasado = document.getElementById("boton-proxima-carta");
-botonHubieraPasado?.addEventListener("click",queHubieraPasado);
-
-// CENTRAR UN DIV SIMPLE EN EL CENTRO
-// MEDIA QUERIES PARA MOBILE (width 100% o menú alargado)
-// COLORES BALATRO
+if (botonHubieraPasado !== null && botonHubieraPasado !== undefined
+    && botonHubieraPasado instanceof HTMLButtonElement) {
+        botonHubieraPasado.addEventListener("click",queHubieraPasado);
+    }
